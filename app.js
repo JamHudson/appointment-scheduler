ab12f757de87b5c4b52337cb4f6e10d2a8556726
JHud <219741840+JamHudson@users.noreply.github.com>
2026-02-10 14:26:54 -0800

    App setup

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-import express from `express`;
+import express from 'express';
 
 
 const app = express();
 
+const PORT = 3001;
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req,res)=> {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running at 
+        http//:localhost:${PORT}`);
+});
+
+app.use(express.static('public'));
